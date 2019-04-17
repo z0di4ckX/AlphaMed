f992c3425783abf842a2cf5ca15168b49f4350a5
Wesly J. Hernandez <ultimatecracker88@gmail.com>
2019-04-17 18:49:51 -0400

    update logIn.java file

--- LogIn.java
+++ LogIn.java
@@ -15,7 +15,7 @@ class LogIn extends App{
             JOptionPane.showMessageDialog(null, "Login successfuly", "Login", JOptionPane.INFORMATION_MESSAGE);
           }
           else {
-            JOptionPane.showMessageDialog(null, "Login fails, please try agen", "Login", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Login failed, please try again", "Login", JOptionPane.ERROR_MESSAGE);
           }
         }while(user.equals("admin") == false && password.equals("1234") == false);
         // This if-else probably has to go outside of menu(). maybe inside of main method, not in the class
